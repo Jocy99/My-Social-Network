135f1a6dfc6c0f82904491ef00a7a93bbe70cb95
Joce <jocelynn4355@gmail.com>
2023-11-05 12:43:44 -0600

    added routes to middleware

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express();
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => {
